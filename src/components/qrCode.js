362379aec97b2486349c472607e25c8dbea48ced
Brian Chung <brianjaychung@gmail.com>
2022-01-12 17:01:38 -0800

    fix url path

--- src/components/qrCode.js
+++ src/components/qrCode.js
@@ -2,9 +2,8 @@ import QRCode from "react-qr-code";
 import { withLDConsumer } from "launchdarkly-react-client-sdk";
 
 //app hosted in public S3 bucket
-const QRURL = `ldsolutions.ldqrapp.org?clientsideid=${window.localStorage.getItem('CLIENTKEY')}`
+const QRURL = `ldsolutions.ldqrapp.org/?clientsideid=${window.localStorage.getItem('CLIENTKEY')}`
 
-console.log(process.env)
 const qrCodeHome = ({ flags, ldClient /*, ...otherProps */ }) => {
   let showFeature = ldClient.variation("reactQRCode");
   
